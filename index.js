96281deb6f16770d8f409b66313ac9043badbeb1
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-10-04 15:03:31 +0200

    name and age

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstName = "Leon";
+let lastName = "Kwizera";
+let middleName = "Magnificat";
+
+let fullName = firstName + " " + middleName + " " + lastName;
+
+console.log("Hello my name is:", fullName);
+
+let myAge = 20;
+let currentYear = 2022;
+let birthYear = currentYear - myAge;
+console.log(birthYear);
